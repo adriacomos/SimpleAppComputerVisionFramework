7d65ddd5148fd84f83423bf95510c06793b02441
capitanminero <adria_comos@hotmail.com>
2015-02-01 17:42:57 +0100

    Paso responsabilidad configuración al llamante (main)

--- SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
+++ SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
@@ -5,12 +5,30 @@
 
 #include "ComputerVisionManager.h"
 
+#include <memory>
+
+using namespace std;
+using namespace cvf;
+
+#include "StrongCornersFeatureTrackerGPU.h"
+#include "UserInterfaceSimpleAnchor.h"
+#include "SingleFeatureTrackerCtrl.h"
+
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//shared_ptr<SURFFeatureStaticMaskTracker> ptrFrame = make_shared<SURFFeatureStaticMaskTracker>( cv::Rect(350,100,75,75), 3 );
+	//shared_ptr<SURFFeatureTrackerCPU> ptrFrame = make_shared<SURFFeatureTrackerCPU>( cv::Rect(350,100,75,75), 3 );
+	shared_ptr<StrongCornersFeatureTrackerGPU> ptrFrame = make_shared<StrongCornersFeatureTrackerGPU>( cv::Rect(350,100,75,75), 3 );
+	shared_ptr<UserInterfaceSimpleAnchor> ui = make_shared<UserInterfaceSimpleAnchor>( ptrFrame );
+	shared_ptr<SingleFeatureTrackerCtrl> singleFeatureTrackerCtrl = make_shared<SingleFeatureTrackerCtrl>( ptrFrame, ui );
 	
 	cvf::ComputerVisionManager cm;
-	cm.startVideoProcessor();
+
+	cm.setFrameProcessorCtrl( singleFeatureTrackerCtrl );
+
+	cm.startVideoProcessorFromFile("MotoGPSlowMotion.avi");
 
 	cm.join();
 
