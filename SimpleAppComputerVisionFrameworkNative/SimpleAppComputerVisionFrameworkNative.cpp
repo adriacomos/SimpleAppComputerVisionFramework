4d4ebee92fa5fd678bb39678535fd309c9065300
AdriaComos <adria_comos@hotmail.com>
2015-02-02 13:31:51 +0100

    Mejora interface librería
    Se ha posibilitado especificar externamente el FrameProcessorCtrl.
    Ahora los datos se deberían acceder desde ese interface

--- SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
+++ SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
@@ -13,14 +13,15 @@ using namespace cvf;
 #include "StrongCornersFeatureTrackerGPU.h"
 #include "UserInterfaceSimpleAnchor.h"
 #include "SingleFeatureTrackerCtrl.h"
+#include "SURFFeatureTrackerCPU.h"
 
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//shared_ptr<SURFFeatureStaticMaskTracker> ptrFrame = make_shared<SURFFeatureStaticMaskTracker>( cv::Rect(350,100,75,75), 3 );
-	//shared_ptr<SURFFeatureTrackerCPU> ptrFrame = make_shared<SURFFeatureTrackerCPU>( cv::Rect(350,100,75,75), 3 );
-	shared_ptr<StrongCornersFeatureTrackerGPU> ptrFrame = make_shared<StrongCornersFeatureTrackerGPU>( cv::Rect(350,100,75,75), 3 );
+	shared_ptr<SURFFeatureTrackerCPU> ptrFrame = make_shared<SURFFeatureTrackerCPU>( cv::Rect(350,100,75,75), 3 );
+	//shared_ptr<StrongCornersFeatureTrackerGPU> ptrFrame = make_shared<StrongCornersFeatureTrackerGPU>( cv::Rect(350,100,75,75), 3 );
 	shared_ptr<UserInterfaceSimpleAnchor> ui = make_shared<UserInterfaceSimpleAnchor>( ptrFrame );
 	shared_ptr<SingleFeatureTrackerCtrl> singleFeatureTrackerCtrl = make_shared<SingleFeatureTrackerCtrl>( ptrFrame, ui );
 	
