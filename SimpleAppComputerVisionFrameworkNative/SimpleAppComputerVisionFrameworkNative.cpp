52e5185f864ba1b582d1634e32a7e96ae7c330f0
AdriaComos <adria_comos@hotmail.com>
2015-02-02 18:08:58 +0100

    Añadida pequeña línea de comando
    Añadida línea de comando para escoger entre device o filchero.

--- SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
+++ SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
@@ -6,6 +6,8 @@
 #include "ComputerVisionManager.h"
 
 #include <memory>
+#include <iostream>
+#include <string>
 
 using namespace std;
 using namespace cvf;
@@ -14,23 +16,58 @@ using namespace cvf;
 #include "UserInterfaceSimpleAnchor.h"
 #include "SingleFeatureTrackerCtrl.h"
 #include "SURFFeatureTrackerCPU.h"
+#include "HistShotBoundaryDetector.h"
 
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//shared_ptr<SURFFeatureStaticMaskTracker> ptrFrame = make_shared<SURFFeatureStaticMaskTracker>( cv::Rect(350,100,75,75), 3 );
+	int device = 0;
+	string fileName = "MotoGPSlowMotion.avi";
+	bool bFromFile = true;;
+
+	for (int i = 0; i < argc; i++) {
+		std::wstring ws = argv[i];
+		if (ws == _T("-d"))
+		{
+			bFromFile = false;
+			i++;
+			if (i < argc) {
+				ws = argv[i];
+				device = stoi(ws);
+			}
+		}
+
+		if (ws == _T("-f"))
+		{
+			i++;
+			if (i < argc) {
+				ws = argv[i];
+				fileName.assign( ws.begin(), ws.end());
+			}
+		}
+
+	}
+
+
+	//shared_ptr<HistShotBoundaryDetector> ptrFrame = make_shared<HistShotBoundaryDetector>( cv::Rect(350,100,75,75), 3 );
 	shared_ptr<SURFFeatureTrackerCPU> ptrFrame = make_shared<SURFFeatureTrackerCPU>( cv::Rect(350,100,75,75), 3 );
 	//shared_ptr<StrongCornersFeatureTrackerGPU> ptrFrame = make_shared<StrongCornersFeatureTrackerGPU>( cv::Rect(350,100,75,75), 3 );
 	shared_ptr<UserInterfaceSimpleAnchor> ui = make_shared<UserInterfaceSimpleAnchor>( ptrFrame );
 	shared_ptr<SingleFeatureTrackerCtrl> singleFeatureTrackerCtrl = make_shared<SingleFeatureTrackerCtrl>( ptrFrame, ui );
 	
+	
 	cvf::ComputerVisionManager cm;
 
 	cm.setFrameProcessorCtrl( singleFeatureTrackerCtrl );
 
-	cm.startVideoProcessorFromFile("MotoGPSlowMotion.avi");
-
+	if (bFromFile) {
+		cm.startVideoProcessorFromFile(fileName);
+	}
+	else {
+		cm.startVideoProcessorFromDevice(device);
+	}
+	
 	cm.join();
 
 	return 0;
