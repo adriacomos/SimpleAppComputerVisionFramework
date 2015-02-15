ae8f55965ee8c1dc395c70dff68d13979258220d
capitanminero <adria_comos@hotmail.com>
2015-02-15 18:41:43 +0100

    Cambio tamaño pruebas FFT

--- SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
+++ SimpleAppComputerVisionFrameworkNative/SimpleAppComputerVisionFrameworkNative.cpp
@@ -54,7 +54,7 @@ void initSingleTrackerWSBD( cvf::ComputerVisionManager &cm, double changeShotThr
 
 void initFFTTracker( cvf::ComputerVisionManager &cm )
 {
-	shared_ptr<IFrameProcessorCtrl> singleFeatureTrackerCtrl = FeatureTrackerFactory::createFFTMatcherTracker( GPU, cv::Rect(350,200,60,60), false, 0 );
+	shared_ptr<IFrameProcessorCtrl> singleFeatureTrackerCtrl = FeatureTrackerFactory::createFFTMatcherTracker( GPU, cv::Rect(350,200,30,30), false, 0 );
 	
 	cm.setFrameProcessorCtrl( singleFeatureTrackerCtrl );
 	
